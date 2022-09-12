91cfe380440a3323e80ea0a74cfdc1c27fa02053
tsepot <tsepotwala@yahoo.com>
2022-09-12 11:51:08 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/*
+ * Write a C program that prints exactly 
+ * Programming is like building a multilingual puzzle, followed by a new line.
+ *
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/*
+ * Write a C program that prints exactly
+ * with proper grammar, but the outcome is a piece of art
+ */
+
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
