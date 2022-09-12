c3ba21755cd83058bc9d260c419b2aba9acf883a
tsepot <tsepotwala@yahoo.com>
2022-09-12 14:05:41 -0700

    update

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - 7-print_tebahpla.c
+ * main - print_tebahpla.c
  *
- * Description: 7-print_tebahpla.c
+ * Description: print_tebahpla.c
  * Return: 0
  */
 int main(void)
 {
 	char c;
+
 	for (c = 'z'; c >= 'a'; c--)
 	{
 		putchar(c);
