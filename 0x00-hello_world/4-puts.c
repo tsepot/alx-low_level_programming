b0c8e31c785a4f028277d375857e15eca9e10a22
tsepot <tsepotwala@yahoo.com>
2022-09-12 11:58:20 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-/*
- * main - print the string in the put function
- * Description: Write a C program that prints exactly
- * Programming is like building a multilingual puzzle, followed by a new line.
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
