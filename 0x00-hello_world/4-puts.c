c8d62cc66532006ee1ef88ce65dcf2a1d403e999
tsepot <tsepotwala@yahoo.com>
2022-09-12 11:54:54 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /*
- * Write a C program that prints exactly 
+ * Description: Write a C program that prints exactly
  * Programming is like building a multilingual puzzle, followed by a new line.
- *
  */
 int main(void)
 {
