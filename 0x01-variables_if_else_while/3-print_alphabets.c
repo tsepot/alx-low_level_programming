52caca0086f489d9e4c85f66a48f0b3dd262eebd
tsepot <tsepotwala@yahoo.com>
2022-09-12 13:35:38 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - 3-print_alphabets
+ * Description: print alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
