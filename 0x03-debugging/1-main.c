62e96ab23f8561a4414c7e0434e4cdabecf10113
Ts3po <tsepotwala@yahoo.com>
2022-09-16 03:17:12 +0200

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - start code
  *
@@ -8,8 +9,12 @@
 int main(void)
 {
 	int i;
+	
 	printf("Infinite loop incoming :(\n");
+	
 	i = 0;
+	
 	printf("Infinite loop avoided! \\o/\n");
+	
 	return (0);
 }
