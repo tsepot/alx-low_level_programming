bbe082797d5786c0673f86d94fb5ce2f94bfcba9
Ts3po <tsepotwala@yahoo.com>
2022-09-16 02:33:49 +0200

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,20 @@
-#include "main.h"
-/**
- * print_most_numbers - check the code.
- *
- * Return: Always 0.
- */
-void print_most_numbers(void)
-{
-	int c = '0';
-
-	while (c <= 9)
-	{
-		if (c != '2' && c != '4')
-		{
-			_putchar(c);
-		}
-	c++;
-	}
-_putchar('\n');
+#include "main.h"                                                                                                  
+/**                                                                                                                
+ * print_most_numbers - check the code.                                                                            
+ *                                                                                                                 
+ * Return: Always 0.                                                                                               
+ */                                                                                                                
+void print_most_numbers(void)                                                                                      
+{                                                                                                                  
+		int c = '0';                                                                                               
+																												   
+		while (c <= 9)                                                                                             
+		{                                                                                                          
+				if (c != '2' && c != '4')                                                                          
+				{                                                                                                  
+						_putchar(c);                                                                               
+				}                                                                                                  
+		c++;                                                                                                       
+		}                                                                                                          
+_putchar('\n');                                                                                                    
 }
