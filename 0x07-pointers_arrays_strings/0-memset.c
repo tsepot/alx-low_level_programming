ce51f9c0876879ef07f4168adbb999d35d2d6338
tsepot <tsepotwala@yahoo.com>
2022-09-27 11:25:51 -0700

    add task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - entry point
+ *
+ * @s: s value
+ * @b: b value
+ * @n: n value
+ *
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x = 0;
+
+	for (x; x < n; x++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
