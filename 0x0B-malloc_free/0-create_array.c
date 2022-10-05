9ec40b12fec4c75648b52e6da5af7b6bfddd3cc7
tsepot <tsepotwala@yahoo.com>
2022-10-05 16:36:06 -0700

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,14 +12,14 @@ char *create_array(unsigned int size, char c)
 	char *my_array;
 	unsigned int i = 0;
 
-	my_array = malloc(size * sizeof(c));
-
+	if (size != 0)
+		my_array = malloc(size * sizeof(c));
+	else
+		return (NULL);
+	if (my_array == NULL)
+		return (NULL);
 	while (i < size)
 	{
-		if (my_array == NULL)
-		{
-			return (NULL);
-		}
 		my_array[i] = c;
 		i++;
 	}
