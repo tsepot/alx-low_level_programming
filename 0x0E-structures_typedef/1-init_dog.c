75822b19e1fd45354bd451a42cf5040bf259d43f
tsepot <tsepotwala@yahoo.com>
2022-10-11 10:07:55 -0700

    update

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - entry
+ * @d: name
+ * @name: dogname
+ * @age: dogage
+ * @owner: dog owner
+ */
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
