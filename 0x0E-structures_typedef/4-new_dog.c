6985b6476cde9ce4987e3c1d6871f3b3f0a138af
tsepot <tsepotwala@yahoo.com>
2022-10-11 10:04:15 -0700

    add task

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,51 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - entry
+ * @name: dogname
+ * @age: dogage
+ * @owner: dog owner
+ * Return: struct dog.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggy;
+	int i, lname, lowner;
+
+	doggy = malloc(sizeof(*doggy));
+	if (doggy == NULL || !(name) || !(owner))
+	{
+		free(doggy);
+		return (NULL);
+	}
+
+	for (lname = 0; name[lname]; lname++)
+		;
+
+	for (lowner = 0; owner[lowner]; lowner++)
+		;
+
+	doggy->name = malloc(lname + 1);
+	doggy->owner = malloc(lowner + 1);
+
+	if (!(doggy->name) || !(doggy->owner))
+	{
+		free(doggy->owner);
+		free(doggy->name);
+		free(doggy);
+		return (NULL);
+	}
+
+	for (i = 0; i < lname; i++)
+		doggy->name[i] = name[i];
+	doggy->name[i] = '\0';
+
+	doggy->age = age;
+
+	for (i = 0; i < lowner; i++)
+		p_dog->owner[i] = owner[i];
+	doggy->owner[i] = '\0';
+
+	return (doggy);
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struture of a dog
+ * @name: dog nam
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+} dog_t;
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
