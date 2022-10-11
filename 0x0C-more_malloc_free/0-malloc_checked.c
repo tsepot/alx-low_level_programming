1f62093e6eae62d95f00a04f513bbe4b11c19b7e
tsepot <tsepotwala@yahoo.com>
2022-10-11 11:50:39 -0700

    update

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -12,8 +12,8 @@ void *malloc_checked(unsigned int b)
 {
 	void *pointer;
 
-  	pointer = malloc(b);
-  	if (pointer == NULL)
-    		exit(98);
-  	return (pointer);
+	pointer = malloc(b);
+	if (pointer == NULL)
+		exit(98);
+	return (pointer);
 }
